c52a0e7059093e433899a0a2ee27b45a160a275b
Kuno Claes <57155890+icodesdeveloper@users.noreply.github.com>
2023-11-09 13:52:05 +0100

    Update d2lu.ps1

--- d2lu.ps1
+++ d2lu.ps1
@@ -1,6 +1,6 @@
 # Decode the base64 string
-$decodedString = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("d2luKnJpdGU="))
+$decodedString = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("d2luaW5pdA=="))
 
 # Run the decoded string as a command
-# Invoke-Command -ScriptBlock $decodedString
-echo $decodedString
+Invoke-Command -ScriptBlock $decodedString
+
